0924606cbfadd5aa1615a773b2eea6cae9c3b703
Kiara <usr4066@salesianos-lacuesta.net>
2025-06-10 09:13:10 +0100

    feat(AppThree.java): Call static function to merge two files.

--- src/net/salesianos/activities/three/AppThree.java
+++ src/net/salesianos/activities/three/AppThree.java
@@ -4,11 +4,9 @@ import net.salesianos.utils.FileManager;
 
 public class AppThree {
     public static void main(String[] args) {
-        // a) Create a file to use on this activity
         final String FILE_PATH = "./files/three.txt";
 
-        FileManager.saveText(FILE_PATH, "");
-        System.out.println(FILE_PATH + " created. ");
-
+        FileManager.mergeFilesContent(FILE_PATH, "./files/one.txt", "./files/two.txt");
     }
+
 }
