4b65bd8a0e54ab32ecb2e7865b688982ee6418f9
Kiara <usr4066@salesianos-lacuesta.net>
2025-06-10 09:12:21 +0100

    feat(FileManager.java): Creation of static function to merge content of two files into one.

--- src/net/salesianos/utils/FileManager.java
+++ src/net/salesianos/utils/FileManager.java
@@ -29,8 +29,7 @@ public class FileManager {
      *
      * @param filePath The path to the text file to be read.
      * @return A string containing the characters and their ASCII values, or an
-     *         error message
-     *         if an IOException occurs.
+     *         error message.
      * @throws IOException If an error occurs while reading the file.
      */
     public static String readTextWithAscii(String filePath) {
@@ -48,5 +47,50 @@ public class FileManager {
             return "Error reading file: " + ioe.getMessage();
         }
         return output.toString();
+    }
+
+    /**
+     * Reads the content of a text file and returns it as a string.
+     *
+     * @param filePath The path to the text file to be read.
+     * @return A string containing the content of the file, or an error message
+     * 
+     * @throws IOException
+     */
+    public static String readText(String filePath) {
+        StringBuilder content = new StringBuilder();
+        try (FileReader reader = new FileReader(filePath)) {
+            int character;
+            while ((character = reader.read()) != -1) {
+                content.append((char) character);
+            }
+        } catch (IOException ioe) {
+            return "Error reading file: " + ioe.getMessage();
+        }
+        return content.toString();
+    }
+
+    /**
+     * Merges the content of two source files into a target file.
+     * If the target file does not exist, it will be created.
+     * 
+     * @param targetFilePath    The file where content will be saved.
+     * @param sourceFilePathOne The first source file to read.
+     * @param sourceFilePathTwo The second source file to read.
+     * 
+     * @throws IOException
+     */
+    public static void mergeFilesContent(String targetFilePath, String sourceFilePathOne, String sourceFilePathTwo) {
+        FileManager.saveText(targetFilePath, "");
+        System.out.println(targetFilePath + " created. ");
+
+        String contentOne = FileManager.readText(sourceFilePathOne);
+        String contentTwo = FileManager.readText(sourceFilePathTwo);
+
+        String mergedContent = "- Contenido del Fichero Uno: " + contentOne + "\n" +
+                "- Contenido del Fichero Dos: " + contentTwo;
+
+        FileManager.saveText(targetFilePath, mergedContent);
+        System.out.println("Content merged and saved to " + targetFilePath);
     }
 }
